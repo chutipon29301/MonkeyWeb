a05b4ed5e1772d823f38605ac6556bff2245dfd7
chutipon29301 <chutiponh@hotmail.com>
2017-05-25 14:04:49 +0700

    Add login function in login.html

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,7 @@
-function loginBtnClick() {
+function loginSubmit() {
 	"use strict";
+
+	console.log("Hello World");
 	var user = document.getElementById("usr");
 	var pwd = document.getElementById("pwd");
 
@@ -8,7 +10,37 @@ function loginBtnClick() {
 		document.getElementById("usrFromGroup").classList.add("has-error");
 	}
 	if (pwd.value.length === 0) {
-		document.getElementById("pwdReq").style.visibility = "visible";
-		document.getElementById("pwdFromGroup").classList.add("has-error");
+		document.getElementById("usrReq").style.visibility = "visible";
+		document.getElementById("usrFromGroup").classList.add("has-error");
 	}
+
+	if (pwd.value.length !== 0 && user.value.length !== 0) {
+		login(parseInt(user.value), pwd.value);
+	}
+}
+
+function login(user, pwd) {
+	"use strict";
+	log("Username:" + user + ",Password:" + pwd);
+	$.post("http://192.168.1.135/post/password", {
+		userID: user,
+		password: pwd
+	}, function (data) {
+		if (data.err) {
+			log("Invalid");
+		} else {
+			console.log(data);
+			if (data.verified) {
+				log("Pass");
+			} else {
+				log("Wrong");
+			}
+		}
+	});
+
+}
+
+function log(text) {
+	"use strict";
+	console.log(text);
 }
