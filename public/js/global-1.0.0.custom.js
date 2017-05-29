fc8ba13e3a570b3c81bb8bd69d249b44bf550f62
admin <admin@Chutipons-MacBook-Pro-2.local>
2017-05-29 23:25:16 +0700

    Add comment into global file

--- public/js/global-1.0.0.custom.js
+++ public/js/global-1.0.0.custom.js
@@ -1,20 +1,39 @@
+/**
+ * Use to log text to console
+ * @param text text to be log in console
+ */
 function log(text) {
 	"use strict";
 	console.log(text);
 }
 
+
+/**
+ * Add variable to document.cookie
+ * @param key variable name to be added
+ * @param value variable value
+ */
 function writeCookie(key, value) {
 	"use strict";
 	document.cookie = key + "=" + value;
 	log("[writeCookie()] -> " + key + "wrote");
 }
 
+/**
+ * Delete variable from document.cookie
+ * @param key variable name to be deleted
+ */
 function deleteCookie(key) {
 	"use strict";
 	document.cookie = key + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 	log("[deleteCookie()] -> " + key + "deleted");
 }
 
+
+/**
+ * Check whether user is login from value store in document.cookie
+ * if user is not login, redirect to login page
+ */
 function checkLogin() {
 	"use strict";
 	var cookie = getCookieDict();
@@ -41,6 +60,10 @@ function checkLogin() {
 
 }
 
+/**
+ * Generate object of document.cookie
+ * @return dict object of document.cookie
+ */
 function getCookieDict() {
 	"use strict";
 	var allcookies = document.cookie;
@@ -53,6 +76,9 @@ function getCookieDict() {
 	return dict;
 }
 
+/**
+ * Logout current user
+ */
 function logout() {
 	"use strict";
 	log("Logout");
