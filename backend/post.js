7dfa627282b2221704b456793d8c7baf5cbe0c48
Pepewitch <skpepe134@gmail.com>
2017-05-31 23:29:30 +0700

    fix next btn bug on regCr file

--- backend/post.js
+++ backend/post.js
@@ -83,6 +83,7 @@ var run=function(app,db){
     addPage("login","/");
     addPage("login");
     addPage("registrationCourse");
+    addPage("registrationHybrid");
     addPage("home");
     addPage("home2");
     addPage("adminAllcourse");
