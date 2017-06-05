c784d66a870dd61d5e32d5c22235f6858f9d27e4
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-06-05 14:25:00 +0700

    Add function for call allCourse

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,5 @@
 /**
- * Get data for generating table by calling function generateHtmlTable
+ * Get data for generating table by calling function generateStudentHtmlTable
  */
 function getAllStudentContent() {
     "use strict";
@@ -8,7 +8,7 @@ function getAllStudentContent() {
             log("[getAllStudentContent()] : post/return => Error");
         } else {
             log("[getAllStudentContent()] : post/return => ");
-            generateHtmlTable(filterData(data.student));
+            generateStudentHtmlTable(filterData(data.student));
         }
     });
 }
@@ -17,7 +17,7 @@ function getAllStudentContent() {
  * Generate Html element from data
  * @param student information to fill in table
  */
-function generateHtmlTable(student) {
+function generateStudentHtmlTable(student) {
     "use strict";
     var table = document.getElementById("allStudentTable");
     for (var i = 0; i < student.length; i++) {
@@ -46,10 +46,57 @@ function generateHtmlTable(student) {
     }
 }
 
-function getTable() {
-
+/**
+ * Generate element for adminAllcourse page
+ */
+function getAllCourseContent() {
+    "use strict";
+    $.post("/post/allCourse", "", function (data) {
+        if (data.err) {
+            log("[getAllCourseContent()] : post/return => Error");
+        } else {
+            log("[getAllCourseContent()] : post/return => ");
+            generateStudentHtmlTable(filterData(data.student));
+        }
+    });
 }
 
+/**
+ * Generate Html element from data
+ * @param student information to fill in table
+ */
+function generateStudentHtmlTable(student) {
+    "use strict";
+    var table = document.getElementById("allStudentTable");
+    for (var i = 0; i < student.length; i++) {
+        var row = table.insertRow(i);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        cell1.innerHTML = "<td>" + student[i].studentID + "</td>";
+        cell2.innerHTML = "<td>" + student[i].nickname + "</td>";
+        cell3.innerHTML = "<td>" + student[i].firstname + "</td>";
+        cell4.innerHTML = "<td>" + student[i].lastname + "</td>";
+        cell5.innerHTML = "<td>" + ((student[i].inCourse) ? "✔" : "✖") + "</td>";
+        cell6.innerHTML = "<td>" + ((student[i].inHybrid) ? "✔" : "✖") + "</td>";
+
+        var clickHandler = function (row) {
+            return function () {
+                log(row.getElementsByTagName("td")[0].innerHTML);
+                writeCookie("monkeyWebAdminAllstudentSelectedUser", row.getElementsByTagName("td")[0].innerHTML);
+                self.location = "/adminStudentprofile";
+            };
+        };
+        row.onclick = clickHandler(row);
+    }
+}
+
+/**
+ * Update allStudent table
+ */
 function optionSelected() {
     var selectBox = document.getElementById("studentMainFilter");
     var selectData = [];
@@ -73,7 +120,7 @@ function optionSelected() {
 /**
  *
  * @param data array of student info
- * @returns {*}
+ * @returns {*} array of student to display in table
  */
 function filterData(data) {
     var selectBox = document.getElementById("studentMainFilter");
@@ -89,7 +136,6 @@ function filterData(data) {
             return data.filter(function (option) {
                 return true;
             });
-            break;
             break;
         default:
             return data;
@@ -123,12 +169,6 @@ function getStudentProfile() {
     });
 }
 
-/**
- * Generate element for adminAllcourse page
- */
-function getAllCourse() {
-    //prepare for generate course for allCoursePage
-}
 
 /**
  * Convert number grade to string grade
--- public/js/drawImage.js
+++ public/js/drawImage.js
@@ -5,69 +5,69 @@ var dateList = ['', 'TUE', 'THU', 'SAT', 'SUN'];
 var timeList = ['', '8-10', '10-12', '13-15', '15-17'];
 var bgColorList = ['black', 'deeppink', 'orange', 'purple', 'red'];
 var textColorList = ['white', 'black', 'black', 'black', 'black'];
-var textTableData = '';
+var textTableData;
 var courseData = [[], [], [], []];
 
 /**
  * Create temp data in courseData Array
  */
- for (var i = 0; i < 4; i++) {
- 	for (var j = 0; j < 4; j++) {
- 		courseData[i][j] = 'MS123a';
- 	}
- }
- log(courseData);
+for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+        courseData[i][j] = 'MS123a';
+    }
+}
+log(courseData);
 
 
 /**
  * Put courseData into tableData array
  */
 for (var i = 0; i < 5; i++) {
-	for (var j = 0; j < 5; j++) {
-		if (i == 0) {
-			tableData[i][j] = dateList[j];
-		} else if(j == 0){
-			tableData[i][j] = timeList[i];
-		} else{
-			tableData[i][j] = courseData[i - 1][j - 1];
-		}
-	}
+    for (var j = 0; j < 5; j++) {
+        if (i === 0) {
+            tableData[i][j] = dateList[j];
+        } else if (j === 0) {
+            tableData[i][j] = timeList[i];
+        } else {
+            tableData[i][j] = courseData[i - 1][j - 1];
+        }
+    }
 }
 
 /**
  * Generate html from tableData array
  */
-textTableData = '<table>';
+textTableData = '<table style="border: solid black;border-collapse: collapse">';
 for (var i = 0; i < tableData.length; i++) {
-	textTableData += '<tr style="height: 72px;color: ' + textColorList[i] + ';font-size:36px;">';
-	for (var j = 0; j < tableData[i].length; j++) {
-		if (i == 0) {
-			textTableData += '<th style="background-color: ' + bgColorList[j] + ';width: 160px;">' + tableData[i][j] + '</th>';
-		} else{
-			textTableData += '<td>' + tableData[i][j] + '</td>'
-		}
-	}
-	textTableData += '</tr>';
+    textTableData += '<tr style="height: 72px;border: solid black;color: ' + textColorList[i] + ';font-size:36px;">';
+    for (var j = 0; j < tableData[i].length; j++) {
+        if (i === 0) {
+            textTableData += '<th style="background-color: ' + bgColorList[j] + ';border: solid black;width: 160px;">' + tableData[i][j] + '</th>';
+        } else {
+            textTableData += '<td style="border: solid black;">' + tableData[i][j] + '</td>'
+        }
+    }
+    textTableData += '</tr>';
 }
 textTableData += '</table>';
 
-var data = 
-'<svg xmlns="http://www.w3.org/2000/svg" width="800" height="360">'+
-'<foreignObject width="100%" height="100%">'+
-'<div xmlns="http://www.w3.org/1999/xhtml">'+
-textTableData +
-'</div>'+
-'</foreignObject>'+
-'</svg>';
+var data =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="800" height="360">' +
+    '<foreignObject width="100%" height="100%">' +
+    '<div xmlns="http://www.w3.org/1999/xhtml">' +
+    textTableData +
+    '</div>' +
+    '</foreignObject>' +
+    '</svg>';
 var DOMURL = window.URL || window.webkitURL || window;
 
 var img = new Image();
 var svg = new Blob([data], {type: 'image/svg+xml'});
 var url = DOMURL.createObjectURL(svg);
 
-img.onload = function() {
-  ctx.drawImage(img, 0, 0);
-  DOMURL.revokeObjectURL(url);
-}
+img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+    DOMURL.revokeObjectURL(url);
+};
 
 img.src = url;
