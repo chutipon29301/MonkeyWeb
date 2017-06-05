ce7cda45d603147e072cbd294c5cf05efbc467d0
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-06-05 09:44:11 +0700

    Seperate function to generate table in adminAllstudent

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,68 +1,88 @@
+/**
+ * Generate element for adminAllstudent page
+ */
 function getAllStudentContent() {
-	"use strict";
-	$.post("/post/allStudent", "", function (data) {
-		if (data.err) {
-			log("[getAllStudent()] : post/return => Error");
-		} else {
-			log("[getAllStudent()] : post/return => ");
-			log(data);
-			var student = data.student;
-			var table = document.getElementById("allStudentTable");
-			for (var i = 0; i < student.length; i++) {
-				var row = table.insertRow(i);
-				var cell1 = row.insertCell(0);
-				var cell2 = row.insertCell(1);
-				var cell3 = row.insertCell(2);
-				var cell4 = row.insertCell(3);
-				var cell5 = row.insertCell(4);
-				var cell6 = row.insertCell(5);
-				cell1.innerHTML = "<td>" + student[i].studentID + "</td>";
-				cell2.innerHTML = "<td>" + student[i].nickname + "</td>";
-				cell3.innerHTML = "<td>" + student[i].firstname + "</td>";
-				cell4.innerHTML = "<td>" + student[i].lastname + "</td>";
-				cell5.innerHTML = "<td>" + ((student[i].inCourse) ? "✔" : "✖") + "</td>";
-				cell6.innerHTML = "<td>" + ((student[i].inHybrid) ? "✔" : "✖") + "</td>";
-
-				var clickHandler = function (row) {
-					return function () {
-						log(row.getElementsByTagName("td")[0].innerHTML);
-						writeCookie("monkeyWebAdminAllstudentSelectedUser", row.getElementsByTagName("td")[0].innerHTML);
-						self.location = "\adminStudentprofile";
-					};
-				};
-				row.onclick = clickHandler(row);
-			}
-		}
-	});
+    "use strict";
+    $.post("/post/allStudent", "", function (data) {
+        if (data.err) {
+            log("[getAllStudent()] : post/return => Error");
+        } else {
+            log("[getAllStudent()] : post/return => ");
+            log(data);
+            generateHtmlTable(data);
+        }
+    });
 }
 
+function generateHtmlTable(data) {
+    "use strict";
+    var student = data.student;
+    var table = document.getElementById("allStudentTable");
+    for (var i = 0; i < student.length; i++) {
+        var row = table.insertRow(i);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        cell1.innerHTML = "<td>" + student[i].studentID + "</td>";
+        cell2.innerHTML = "<td>" + student[i].nickname + "</td>";
+        cell3.innerHTML = "<td>" + student[i].firstname + "</td>";
+        cell4.innerHTML = "<td>" + student[i].lastname + "</td>";
+        cell5.innerHTML = "<td>" + ((student[i].inCourse) ? "✔" : "✖") + "</td>";
+        cell6.innerHTML = "<td>" + ((student[i].inHybrid) ? "✔" : "✖") + "</td>";
+
+        var clickHandler = function (row) {
+            return function () {
+                log(row.getElementsByTagName("td")[0].innerHTML);
+                writeCookie("monkeyWebAdminAllstudentSelectedUser", row.getElementsByTagName("td")[0].innerHTML);
+                self.location = "/adminStudentprofile";
+            };
+        };
+        row.onclick = clickHandler(row);
+    }
+}
+
+/**
+ * Generate element for studentProfile page
+ */
 function getStudentProfile() {
-	var cookie = getCookieDict();
-	var studentID = cookie.monkeyWebAdminAllstudentSelectedUser;
-	document.getElementById("studentID").innerHTML = "ID: " + studentID;
-	$.post("post/studentProfile", {
-		studentID: studentID,
-	}, function (data) {
-		if (data.err) {
-			log("Invalid");
-		} else {
-			log(data);
-			document.getElementById("studentName").innerHTML = data.firstname + " (" + data.nickname + ") " + data.lastname;
-			document.getElementById("studentLevel").innerHTML = "LEVEL: " + getLetterGrade(data.grade);
-			document.getElementById("studentState").innerHTML = "STAGE: " + data.registrationState;
-			document.getElementById("studentStatus").innerHTML = "STATUS: " + data.status;
-		}
-	});
+    var cookie = getCookieDict();
+    /** @namespace cookie.monkeyWebAdminAllstudentSelectedUser */
+    var studentID = cookie.monkeyWebAdminAllstudentSelectedUser;
+    document.getElementById("studentID").innerHTML = "ID: " + studentID;
+    $.post("post/studentProfile", {
+        studentID: studentID
+    }, function (data) {
+        if (data.err) {
+            log("Invalid");
+        } else {
+            log(data);
+            document.getElementById("studentName").innerHTML = data.firstname + " (" + data.nickname + ") " + data.lastname;
+            document.getElementById("studentLevel").innerHTML = "LEVEL: " + getLetterGrade(data.grade);
+            document.getElementById("studentState").innerHTML = "STAGE: " + data.registrationState;
+            document.getElementById("studentStatus").innerHTML = "STATUS: " + data.status;
+        }
+    });
 }
 
+/**
+ * Fenerate element for adminAllcourse page
+ */
 function getAllCourse() {
 
 }
 
+/**
+ * Convert number grade to string grade
+ * @param grade in form for number
+ * @returns {string} grade letter
+ */
 function getLetterGrade(grade) {
-	if (grade <= 6) {
-		return "P" + grade;
-	} else {
-		return "M" + (grade - 6);
-	}
+    if (grade <= 6) {
+        return "P" + grade;
+    } else {
+        return "M" + (grade - 6);
+    }
 }
