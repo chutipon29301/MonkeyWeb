210829148b029a925fd65e8ad1dc0686818c8943
chiangsk135 <chiang_sk135@hotmail.com>
2017-06-07 11:55:15 +0700

    Fix port

--- backend/post.js
+++ backend/post.js
@@ -220,7 +220,7 @@ var run=function(app,db){
                 if(result.position=="student"){
                     output=result.student;
                     var request=require("request");
-                    request.post("http://localhost:8080/post/name",{form:{userID:studentID}},function(err,response,body){
+                    request.post("http://localhost/post/name",{form:{userID:studentID}},function(err,response,body){
                         body=JSON.parse(body);
                         output=Object.assign(output,body);
                         output.courseID=[];
