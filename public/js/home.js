d673091bb65b482d898b8dee48e85fc66d8d5b33
Nuttapong.P <taoangle009@hotmail.com>
2017-06-16 17:32:43 +0700

    change slideshow method

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,62 @@
-function slideShow() {
-    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
-    Galleria.run($('.galleria'),{extend:function(){
-        this.setOptions('transition','fade');
-//		  this.play(3000);
-    }});
+const gal = $('.galleria');
+let autoPlay = false;
+let ind = '';
+
+function slideShow(mode) {
+    const testAutoplay = $.get('images/news/1.png', function (data, status) {
+        // autoPlay = true;
+        // ind = 'a';
+    }).success(()=>{
+        return true;
+        // autoPlay = true;
+    }).error(()=>{
+        // autoPlay = false;
+        return false;
+    });
+
+    log(testAutoplay.statusText === "Not Found");
+
+    testAutoplay.then((isAutoPlay)=>{
+        let promise = [];
+        for (let i = 1;;i++){
+            let loop = true;
+            log('in loop');
+            promise.append($.get('images/news/' + ind + i + '.PNG', function (data, status) {
+                loop = false;
+                gal.append('<img src="images/news/' + ind + i + '.PNG">');
+                log('in auto' + i);
+            }));
+            if (loop) break;
+        }
+        Promise.all(promise).then(()=>{
+            Galleria.loadTheme('galleria/themes/' + mode + '/galleria.' + mode + '.min.js');
+            Galleria.run($('.galleria'), {
+                extend: function () {
+                    this.setOptions('transition', 'fade');
+                    if (autoPlay) {
+                        this.play(5000);
+                    }
+                }
+            });
+        });
+    });
+    // for (let i = 1; ; i++) {
+    //     let loop = true;
+    //     log('in loop');
+    //     $.get('images/news/' + ind + i + '.PNG', function (data, status) {
+    //         loop = false;
+    //         gal.append('<img src="images/news/' + ind + i + '.PNG">');
+    //         log('in auto' + i);
+    //     });
+    //     if (loop) break;
+    // }
+    // Galleria.loadTheme('galleria/themes/' + mode + '/galleria.' + mode + '.min.js');
+    // Galleria.run($('.galleria'), {
+    //     extend: function () {
+    //         this.setOptions('transition', 'fade');
+    //         if (autoPlay) {
+    //             this.play(5000);
+    //         }
+    //     }
+    // });
 }
