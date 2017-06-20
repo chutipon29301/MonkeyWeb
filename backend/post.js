33f718c31094082fa23c79e6e1cc91d46f1cf108
chiangsk135 <chiang_sk135@hotmail.com>
2017-06-20 15:53:11 +0700

    Add changeStatus method

--- backend/post.js
+++ backend/post.js
@@ -142,6 +142,23 @@ module.exports=function(app,db){
             else res.send({status:result.tutor.status});
         });
     });
+    //OK {userID,status} return {}
+    post("/post/changeStatus",function(req,res){
+        var userID=parseInt(req.body.userID);
+        var status=req.body.status;
+        findUser(res,userID,{},function(result){
+            if(result.position=="student"){
+                userDB.updateOne({_id:userID},{$set:{"student.status":status}},function(){
+                    res.send({});
+                });
+            }
+            else{
+                userDB.updateOne({_id:userID},{$set:{"tutor.status":status}},function(){
+                    res.send({});
+                });
+            }
+        });
+    });
 
 
     // Student Information
