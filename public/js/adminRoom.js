79a3175e8e179b5317f3d91c2b5593ee19c16eff
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-06-26 09:17:01 +0700

    Add rx to admin room

--- public/js/adminRoom.js
+++ public/js/adminRoom.js
@@ -1,3 +1,62 @@
+let app = angular.module("tableRoom", ['rx']);
+
+let tableGenerator = ($scope) => {
+    $scope.roomDayList = [{
+        room: 1,
+        courseName: "CHS123z",
+        noStudent: 10,
+        full: 50,
+        courseID: "5937b91a5201290940356275"
+    }, {
+        courseName: "PHS3a",
+        room: 2,
+        noStudent: 20,
+        full: 50,
+        courseID: "5937b9ce5201290940356279"
+    }, {
+        room: 3,
+        courseName: "PHS4a",
+        noStudent: 30,
+        full: 50,
+        courseID: "5937baa6520129094035627e"
+    }, {
+        room: 4,
+        courseName: "PHS6x",
+        noStudent: 30,
+        full: 50,
+        courseID: "5937bb085201290940356282"
+    }];
+
+    $scope.tableRowClick = (courseID) => {
+        writeCookie("monkeyWebAdminAllcourseSelectedCourseID", courseID);
+        self.location = "/adminCoursedescription";
+    };
+
+    var source = Rx.Observable.range(1, 5);
+
+    var subscription = source.subscribe(
+        x => console.log('onNext: ' + x),
+        e => console.log('onError: ' + e.message),
+        () => console.log('onCompleted'));
+};
+
+app.controller("tue", function ($scope){
+    tableGenerator($scope);
+});
+
+app.controller("thu", function ($scope){
+    tableGenerator($scope);
+});
+
+app.controller("sat", function ($scope){
+    tableGenerator($scope);
+});
+
+app.controller("sun", function ($scope){
+    tableGenerator($scope);
+});
+
+
 function showRoom(evt, cityName) {
     // Declare all variables
     let i, tabcontent, tablinks;
