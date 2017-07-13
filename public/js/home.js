61b42898e0197b6da26bae39e399aeef60265d25
Nuttapong <taoangle009@gmail.com>
2017-07-13 15:15:31 +0700

    finished home admin

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,4 @@
+// function for show slide
 function slideShow(type, mode, autoPlay) {
     let finish = function (n) {
         const gal = $('.galleria');
@@ -24,6 +25,66 @@ function slideShow(type, mode, autoPlay) {
     };
     recur(1);
 }
+// function for adminHome
 function uploadSlide() {
     $("#uploadModal").modal();
+}
+function showSlide() {
+    $('body').dblclick(function () {
+        writeCookie("slideType", "nclassic");
+        location.reload();
+    });
+    let cookie = getCookieDict();
+    if (cookie.slideType !== undefined) {
+        let txt = cookie.slideType;
+        let type = txt.slice(0, 1);
+        let style = txt.slice(1);
+        slideShow(type, style, true);
+    } else {
+        writeCookie("slideType", "nclassic");
+        location.reload();
+    }
+}
+function portrait() {
+    writeCookie("slideType", "pclassic");
+    location.reload();
+}
+function landscape() {
+    writeCookie("slideType", "lclassic");
+    location.reload();
+}
+function news() {
+    writeCookie("slideType", "nclassic");
+    location.reload();
+}
+function full() {
+    writeCookie("slideType", "nfullscreen");
+    location.reload();
+}
+function upNews() {
+    //noinspection JSUnresolvedVariable
+    let ufile = $('#file-1');
+    let ext = ufile.val().split('.').pop().toLowerCase();
+    if ($.inArray(ext, ['png', 'jpg', 'jpeg']) === -1) {
+        alert('กรุณาอัพไฟล์ .jpg, .jpeg หรือ .png เท่านั้น');
+    } else {
+        let files = ufile.get(0).files;
+        if (files.length > 0) {
+            let formData = new FormData();
+            for (let i = 0; i < files.length; i++) {
+                let file = files[i];
+                formData.append('file[]', file, file.name);
+            }
+            $.ajax({
+                url: 'post/updateStudentSlideshow',
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (data) {
+                    $('#uploadModal').modal('hide');
+                }
+            });
+        }
+    }
 }
