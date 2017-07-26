6ddc10b1361c98c102cc880774e85d5d4358e3fd
Nuttapong <taoangle009@gmail.com>
2017-07-26 12:50:38 +0700

    finished comment for tutor

--- public/js/tutorCommentStudent.js
+++ public/js/tutorCommentStudent.js
@@ -27,3 +27,20 @@ function postComment() {
         } else alert("Please Select Correct Name")
     } else alert("Please Input Student Name");
 }
+function showComment() {
+    let ID = $('#search-box .typeahead').typeahead("getActive");
+    if (ID !== undefined) {
+        if (ID.length > 5) {
+            ID = ID.slice(-6, -1);
+            $.post("post/listStudentComment", { studentID: ID }, function (data, status) {
+                for (let i = data.comment.length - 1; i > -1; i--) {
+                    $.post("post/name", { userID: data.comment[i].from }).then((info) => {
+                        let day = moment(data.comment[i].timestamp, "x").format("DD MMM");
+                        $("#commentList").append("<h4>" + info.nickname + " (" + day + ")</h4>");
+                        $("#commentList").append("<p>- " + data.comment[i].message + "</p>");
+                    });
+                }
+            });
+        } else alert("Please Select Correct Name")
+    } else alert("Please Input Student Name");
+}
