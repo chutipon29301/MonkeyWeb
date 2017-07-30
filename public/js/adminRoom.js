a24fe8983cbf1bba8f301f868a5e84fc546c97f6
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-07-30 08:00:11 +0700

    remove olde function for calling tab

--- public/js/adminRoom.js
+++ public/js/adminRoom.js
@@ -106,18 +106,18 @@ let tableGenerator = ($scope, $http, time) => {
         }
     });
 
-    $scope.tableRowClick = (courseID) => {
-        if (courseID !== "Hybrid") {
-            writeCookie("monkeyWebAdminAllcourseSelectedCourseID", courseID);
-            self.location = "/adminCoursedescription";
-        } else {
-            for (let i = 0; i < $scope.roomDayList.length; i++) {
-                if ($scope.roomDayList[i].property === "expandable") {
-                    $scope.roomDayList[i].isHidden = !$scope.roomDayList[i].isHidden
-                }
-            }
-        }
-    };
+    // $scope.tableRowClick = (courseID) => {
+    //     if (courseID !== "Hybrid") {
+    //         writeCookie("monkeyWebAdminAllcourseSelectedCourseID", courseID);
+    //         self.location = "/adminCoursedescription";
+    //     } else {
+    //         for (let i = 0; i < $scope.roomDayList.length; i++) {
+    //             if ($scope.roomDayList[i].property === "expandable") {
+    //                 $scope.roomDayList[i].isHidden = !$scope.roomDayList[i].isHidden
+    //             }
+    //         }
+    //     }
+    // };
 
 };
 
@@ -185,26 +185,26 @@ app.controller("sun15", function ($scope, $http) {
     });
 });
 
-function showRoom(evt, cityName) {
-    // Declare all variables
-    let i, tabcontent, tablinks;
+// function showRoom(evt, cityName) {
+//     // Declare all variables
+//     let i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
 
 function indexOfObjectInArray(array, key) {
     for (let i = 0; i < array.length; i++) {
