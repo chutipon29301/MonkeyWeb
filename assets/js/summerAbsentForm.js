10f85ef42c115e65fe2c72dc7417f5e3351b6c02
Nuttapong <taoangle009@gmail.com>
2017-09-11 15:05:28 +0700

    fix summer present bug

--- assets/js/summerAbsentForm.js
+++ assets/js/summerAbsentForm.js
@@ -54,11 +54,19 @@ $(document).ready(function () {
                     day.push(temp.valueOf());
                 }
                 $.post("post/addStudentAbsenceModifier", { studentID: ID, reason: "ลา", sender: $("#sender").val(), day: day }).then((data) => {
-                    self.location = 'summerReceipt';
+                    if ($(".btn-info").length > 0) {
+                        for (let i = 0; i < $(".btn-info").length; i++) {
+                            let temp = moment().hour($(".btn-info")[i].id.slice(4, 6)).minute(0).date($(".btn-info")[i].id.slice(1, 3)).month(9);
+                            pre.push(temp.valueOf());
+                        }
+                        log(pre)
+                        $.post("post/addStudentAbsenceModifier", { studentID: ID, reason: "เพิ่ม", sender: $("#sender").val(), day: pre }).then((data) => {
+                            self.location = 'summerReceipt';
+                        })
+                    } else self.location = 'summerReceipt';
                 })
             } else alert("กรุณาใส่ชื่อผู้ส่ง");
-        } else if ($(".btn-info").length > 0) {
-            if ($("#sender").val().length > 0) {
+            if ($(".btn-info").length > 0) {
                 for (let i = 0; i < $(".btn-info").length; i++) {
                     let temp = moment().hour($(".btn-info")[i].id.slice(4, 6)).minute(0).date($(".btn-info")[i].id.slice(1, 3)).month(9);
                     pre.push(temp.valueOf());
@@ -67,7 +75,7 @@ $(document).ready(function () {
                 $.post("post/addStudentAbsenceModifier", { studentID: ID, reason: "เพิ่ม", sender: $("#sender").val(), day: pre }).then((data) => {
                     self.location = 'summerReceipt';
                 })
-            } else alert("กรุณาใส่ชื่อผู้ส่ง");
+            }
         } else self.location = 'summerReceipt';
     })
 });
