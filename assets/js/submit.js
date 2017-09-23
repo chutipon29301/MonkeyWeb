0af08c5043e102e461f663fa7ecbd874ddc173ff
Witchayut Jongpattanasombut <skpepe134@gmail.com>
2017-09-23 11:01:02 +0700

    add conference post method

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,5 +1,5 @@
-var cookie
-
+var cookie;
+var studentProfile;
 $(document).ready(function(){
     cookie = getCookieDict()
     if(cookie.courseID){
@@ -37,6 +37,13 @@ $(document).ready(function(){
             $('#reason').show()
         }
     })
+    $.post('post/studentProfile',{studentID:parseInt(cookie.monkeyWebUser)},(data)=>{
+        if(data.err){
+            alert('ขาดการเชื่อมต่อจากเซิร์ฟเวอร์ กรุณาลองใหม่อีกครั้ง')
+            self.location = '/registrationCourse'
+        }
+        studentProfile = data;
+    })
 })
 
 function fillTable(course,option){
@@ -65,6 +72,12 @@ function confirm(){
     var promise = []
     if($('#check').is(':checked') || $('#reason').val().length>3){
         $.post('post/listConference',{},(data)=>{
+            var conferenceID;
+            for(let i in data){
+                if(data[i].name.indexOf((studentProfile.grade>6)?'s':'p') && data[i].name.indexOf((studentProfile.grade>6)?(''+studentProfile.grade-6):(''+studentProfile.grade))){
+                    conferenceID = data[i].conferenceID
+                }
+            }
             promise.push($.post('post/addStudentCourse',{studentID : parseInt(cookie.monkeyWebUser),courseID : cookie.courseID}))
             if(cookie.Hybrid){
                 for(let i in cookie.Hybrid){
@@ -79,7 +92,7 @@ function confirm(){
             promise.push(
                 $.post('post/addStudentToConference',
                     {
-                        //conferenceID: something
+                        conferenceID: conferenceID
                         studentID : parseInt(cookie.monkeyWebUser),
                         isAttended : $('#check').is(':checked')
                     }
