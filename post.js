337a859583b465db7664e83eb2ad8bbb4783aec8
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-09-23 21:57:19 +0700

    Add skilldb function

--- post.js
+++ post.js
@@ -19,6 +19,7 @@ module.exports=function(app,db){
     var userDB=db.collection("user");
     var conferenceDB=db.collection("conference");
     var studentHybridDB = db.collection("hybridStudent");
+    var studentSkillDB = db.collection("skillStudent");
 
     var gradeBitToString=function(bit){
         var output="",p=false,s=false;
@@ -1850,7 +1851,7 @@ module.exports=function(app,db){
     });
 
     /**
-     * Post method for adding hybird day to quarter
+     * Post method for adding hybrid day to quarter
      * req.body = {
      *      quarter: 4
      *      year: 2017
@@ -1872,8 +1873,8 @@ module.exports=function(app,db){
         serverDate.setMonth(reqDate.getMonth());
         serverDate.setFullYear(reqDate.getFullYear());
         quarterDB.findOne({
-            year: req.body.year,
-            quarter: req.body.quarter
+            year: parseInt(req.body.year),
+            quarter: parseInt(req.body.quarter)
         }).then(data => {
             studentHybridDB.insertOne({
                 quarterID: data._id,
@@ -1907,7 +1908,7 @@ module.exports=function(app,db){
      * }
      * res.body = [
      *      day: 34320000
-     *      hybirdID: 49fjf8weijrfsfs4
+     *      hybridID: 49fjf8weijrfsfs4
      * ]
      */
     post("/post/v1/listHybridDayInQuarter", function(req, res){
@@ -1943,7 +1944,7 @@ module.exports=function(app,db){
     });
 
     /**
-     * Post method for adding stuednt to hybrid day
+     * Post method for adding student to hybrid day
      * req.body = {
      *      hybridID: miagjngoajew934jr3432e3
      *      studentID: 15999
@@ -1973,7 +1974,7 @@ module.exports=function(app,db){
     });
 
     /**
-     * Post method for list time of stuednt in hybrid day
+     * Post method for list time of student in hybrid day
      * req.body = {
      *      studentID: 15999
      *      quarter: 4
@@ -1986,7 +1987,7 @@ module.exports=function(app,db){
      *      }
      * ]
      */
-    post("/post/v1/listStudentHybird", function(req, res){
+    post("/post/v1/listStudentHybrid", function(req, res){
         if (req.body.studentID === undefined || req.body.quarter === undefined || req.body.year === undefined) {
             res.status(400).send({
                 err: -1,
@@ -2020,7 +2021,188 @@ module.exports=function(app,db){
                     }
                 }
                 for(let i = 0; i < result.length; i++){
-                    result[i].hybirdID = result[i]._id;
+                    result[i].hybridID = result[i]._id;
+                    delete result[i]._id;
+                    delete result[i].student;
+                    delete result[i].quarterID;
+                }
+                res.status(200).send(result);
+            });
+        });
+    });
+
+    /**
+     * Post method for adding skill day to quarter
+     * req.body = {
+     *      quarter: 4
+     *      year: 2017
+     *      day: 1023004020
+     * }
+     * res.body = "OK"
+     */
+    post("/post/v1/addSkillDayToQuarter", function (req, res) {
+        if (req.body.quarter === undefined || req.body.year === undefined  || req.body.day === undefined) {
+            return res.status(400).send({
+                err: -1,
+                msg: "Bad Request"
+            });
+        }
+        var reqDate = new Date(parseInt(req.body.day));
+        var serverDate = new Date(0);
+        serverDate.setHours(reqDate.getHours());
+        serverDate.setDate(reqDate.getDate());
+        serverDate.setMonth(reqDate.getMonth());
+        serverDate.setFullYear(reqDate.getFullYear());
+        quarterDB.findOne({
+            year: parseInt(req.body.year),
+            quarter: parseInt(req.body.quarter)
+        }).then(data => {
+            studentSkillDB.insertOne({
+                quarterID: data._id,
+                day: serverDate.getTime(),
+                student: []
+            }, function (err, result) {
+                if (err) {
+                    switch (err.code) {
+                        case 11000:
+                            return res.status(501).send({
+                                err: "Data already exist",
+                                msg: err.msg
+                            });
+                        default:
+                            return res.status(500).send({
+                                err: -1,
+                                msg: "Internal Server Error"
+                            });
+                    }
+                }
+                res.status(200).send("OK");
+            });
+        });
+    });
+
+    /**
+     * Post method for listing all skill in quarter
+     * req.body = {
+     *      quarter: 4
+     *      year: 2017
+     * }
+     * res.body = [
+     *      day: 34320000
+     *      skillID: 49fjf8weijrfsfs4
+     * ]
+     */
+    post("/post/v1/listSkillDayInQuarter", function(req, res){
+        if (req.body.quarter === undefined || req.body.year === undefined) {
+            return res.status(400).send({
+                err: -1,
+                msg: "Bad Request"
+            });
+        }
+        quarterDB.findOne({
+            year: parseInt(req.body.year),
+            quarter: parseInt(req.body.quarter)
+        }).then(data => {
+            studentSkillDB.find({
+                quarterID: data._id
+            }).toArray(function(err ,result){
+                if(err){
+                    res.status(500).send({
+                        err: 0,
+                        msg: "Internal server error"
+                    });
+                }
+                for(let i = 0; i < result.length;i++){
+                    result[i].skillID = result[i]._id;
+                    delete result[i]._id;
+                    delete result[i].quarterID
+                    delete result[i].student
+                }
+                console.log(result);
+                res.status(200).send(result);
+            });
+        });
+    });
+
+    /**
+     * Post method for adding student to skill day
+     * req.body = {
+     *      skillID: miagjngoajew934jr3432e3
+     *      studentID: 15999
+     *      subject: 'M'
+     * }
+     * res.body = "OK"
+     */
+    post("/post/v1/addSkillStudent", function (req, res) {
+        if (req.body.skillID === undefined || req.body.studentID === undefined || req.body.subject === undefined) {
+            res.status(400).send({
+                err: -1,
+                msg: "Bad Request"
+            });
+        }
+        studentSkillDB.update({
+            _id: ObjectID(req.body.skillID)
+        }, {
+                $push: {
+                    student: {
+                        studentID: parseInt(req.body.studentID),
+                        subject: req.body.subject
+                    }
+                }
+            }
+        );
+        res.status(200).send("OK")
+    });
+
+    /**
+     * Post method for list time of student in skill day
+     * req.body = {
+     *      studentID: 15999
+     *      quarter: 4
+     *      year: 2017
+     * }
+     * res.body = [
+     *      {
+     *          day: 43959400000
+     *          skillID: 'kiq034krmif035g'
+     *      }
+     * ]
+     */
+    post("/post/v1/listStudentSkill", function(req, res){
+        if (req.body.studentID === undefined || req.body.quarter === undefined || req.body.year === undefined) {
+            res.status(400).send({
+                err: -1,
+                msg: "Bad Request"
+            });
+        }
+        quarterDB.findOne({
+            year: parseInt(req.body.year),
+            quarter: parseInt(req.body.quarter)
+        }).then(data => {
+            studentSkillDB.find({
+                student: {
+                    $elemMatch: {
+                        studentID: parseInt(req.body.studentID)
+                    }
+                },
+                quarterID: data._id
+            }).toArray(function(err, result){
+                if (err) {
+                    switch (err.code) {
+                        case 11000:
+                            return res.status(501).send({
+                                err: "Data aready exist",
+                                msg: err.msg
+                            });
+                        default:
+                            return res.status(500).send({
+                                err: -1,
+                                msg: "Internal Server Error"
+                            });
+                    }
+                }
+                for(let i = 0; i < result.length; i++){
+                    result[i].skillID = result[i]._id;
                     delete result[i]._id;
                     delete result[i].student;
                     delete result[i].quarterID;
