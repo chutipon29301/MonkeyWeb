d149cd69fd3e5fb0420dcfa297397675b8ab327a
Nuttapong <taoangle009@gmail.com>
2017-09-27 20:59:38 +0700

    Finished student absent form

--- assets/js/studentAbsentForm.js
+++ assets/js/studentAbsentForm.js
@@ -1,3 +1,6 @@
+// var for summary data
+var remainMathAbsent = 0;
+var remainPhyAbsent = 0;
 $(document).ready(function () {
     // get studentID
     let cookie = getCookieDict()
@@ -26,6 +29,7 @@ $(document).ready(function () {
                         hybrid.push(fhb[i]);
                     }
                     fillButt(course, hybrid);
+                    fillSummary(ID, hybrid);
                 })
             })
         })
@@ -33,11 +37,13 @@ $(document).ready(function () {
     // set date picker
     $("#datePicker").datetimepicker({
         format: "DD/MM/YYYY",
-        daysOfWeekDisabled: [1, 3, 5]
+        daysOfWeekDisabled: [1, 3, 5],
+        minDate: moment()
     });
     // add event when pick date
     $("#datePicker").on("dp.change", function () {
         fillButt(course, hybrid);
+        fillSummary(ID, hybrid);
     })
     // toggle pick button
     $(".btn-default").click(function () {
@@ -45,7 +51,69 @@ $(document).ready(function () {
             $(this).toggleClass("btn-default btn-success")
         }
     })
+    // add event when submit
+    $("#submit").click(function () {
+        if ($(".btn-success").length > 0) {
+            if ($("#reasonInput").val()) {
+                if ($("#senderInput").val()) {
+                    let state = true;
+                    for (let i = 0; i < $(".btn-success").length; i++) {
+                        if (state) {
+                            if ($($(".btn-success")[i]).html().slice(5) == "PH" && remainPhyAbsent <= 0) {
+                                state = false;
+                                alert("ลา FHB:P ครบจำนวนแล้ว");
+                            } else if ($($(".btn-success")[i]).html().slice(5) == "M" && remainMathAbsent <= 0) {
+                                state = false;
+                                alert("ลา FHB:M ครบจำนวนแล้ว");
+                            }
+                        }
+                    }
+                    if (state) {
+                        log("Send data =>");
+                        sendData(ID);
+                    }
+                } else alert("กรุณาใส่ชื่อผู้แจ้ง")
+            } else alert("กรุณาใส่เหตุผลในการลา");
+        } else alert("กรุณาเลือกวิชาที่ต้องการลา");
+    })
 });
+// func for send data to server
+function sendData(ID) {
+    let pickDate = $('#datePicker').data('DateTimePicker').date();
+    let state = true;
+    let time = [];
+    if (emer(pickDate)) {
+        state = confirm("ต้องการลาฉุกเฉิน");
+    }
+    if (state) {
+        let day = moment().date(pickDate.date()).month(pickDate.month()).year(pickDate.year()).minute(0).second(0).millisecond(0);
+        if (pickDate.day() == 2 || pickDate.day() == 4) {
+            time.push(parseInt(day.hour(17).valueOf()));
+        } else {
+            for (let i = 0; i < $(".btn-success").length; i++) {
+                time.push(parseInt(day.hour($($(".btn-success")[i]).attr("id").slice(3)).valueOf()));
+            }
+        }
+    }
+    log(time)
+    $.post("post/addStudentAbsenceModifier", { studentID: ID, day: time, reason: $("#reasonInput").val(), sender: $("#senderInput").val() }, function (data) {
+        location.reload();
+    })
+}
+function emer(day) {
+    let now = moment().minute(0).second(0).millisecond(0);
+    let picked = moment().year(day.year()).month(day.month()).date(day.date()).hour(18).minute(0).second(0).millisecond(0);
+    let aDay = 24 * 60 * 60 * 1000;
+    if (day.day() == 0) {
+        if (picked.valueOf() - now.valueOf() > aDay * 2) {
+            return false;
+        } else return true;
+    } else {
+        if (picked.valueOf() - now.valueOf() > aDay) {
+            return false;
+        } else return true;
+    }
+}
 // func for add data to butt when pick date
 function fillButt(course, fhb) {
     $(".selector").html("&nbsp;").addClass("disabled btn-default").removeClass("btn-success");
@@ -73,4 +141,47 @@ function fillButt(course, fhb) {
             }
         }
     }
+}
+// func for fill summary data
+function fillSummary(ID, fhb) {
+    let allPhyAbsent = 0;
+    let allMathAbsent = 0;
+    remainPhyAbsent = 0;
+    remainMathAbsent = 0;
+    $("#summaryPhy").empty().html("FHB:Physic&nbsp;");
+    $("#summaryMath").empty().html("FHB:Math&nbsp;");
+    for (let i = 0; i < fhb.length; i++) {
+        if (fhb[i].subject === "PH") {
+            remainPhyAbsent += 3;
+        } else {
+            remainMathAbsent += 3;
+        }
+    }
+    allPhyAbsent = remainPhyAbsent;
+    allMathAbsent = remainMathAbsent;
+    let pickDate = $('#datePicker').data('DateTimePicker').date();
+    let pastDate = moment().day(pickDate.day()).month(pickDate.month() - 3).year(pickDate.year()).hour(0).minute(0).second(0).millisecond(0);
+    $.post("post/listStudentAttendanceModifierByStudent", { studentID: ID, start: pastDate.valueOf() }, function (data) {
+        // log(data)
+        for (let i = 0; i < data.modifier.length; i++) {
+            if (data.modifier[i].type === "absence") {
+                if (data.modifier[i].day <= pickDate.valueOf()) {
+                    if (data.modifier[i].reason != "ลา" && data.modifier[i].reason != "เพิ่ม") {
+                        // log(data.modifier[i])
+                        for (let j = 0; j < fhb.length; j++) {
+                            if (moment(fhb[j].day).day() == moment(data.modifier[i].day).day() && moment(fhb[j].day).hour() == moment(data.modifier[i].day).hour()) {
+                                (fhb[j].subject === "PH") ? remainPhyAbsent -= 1 : remainMathAbsent -= 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (let i = 0; i < allMathAbsent; i++) {
+            $("#summaryMath").append("<span class='glyphicon glyphicon-record' style='color:" + ((i >= remainMathAbsent) ? "red" : "green") + "'></span>");
+        }
+        for (let i = 0; i < allPhyAbsent; i++) {
+            $("#summaryPhy").append("<span class='glyphicon glyphicon-record' style='color:" + ((i >= remainPhyAbsent) ? "red" : "green") + "'></span>");
+        }
+    })
 }
