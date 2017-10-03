accc07260ec801bb8a89c313b9677b7a5cb4cd9b
Nuttapong <taoangle009@gmail.com>
2017-10-03 23:00:12 +0700

    finished addForm

--- assets/js/studentAddForm.js
+++ assets/js/studentAddForm.js
@@ -1,143 +1,121 @@
 $(document).ready(function () {
+    // get studentID
     let cookie = getCookieDict();
-    // add time picker
-    $('#datetimepicker1').datetimepicker({
-        format: 'DD/MM/YYYY',
-        daysOfWeekDisabled: [1, 3, 5]
+    let ID = cookie.monkeyWebUser;
+    // var for year and Q
+    let year = 2017;
+    let quarter = 4;
+    // Param
+    let course = [];
+    let fhb = [];
+    // set date picker
+    $("#datePicker").datetimepicker({
+        format: "DD/MM/YYYY",
+        daysOfWeekDisabled: [1, 3, 5],
+        minDate: moment()
     });
-    // show name
-    $.post('post/name', { userID: cookie.monkeyWebUser }, function (data, status) {
-        $('#name').append(' ' + data.nickname + ' ' + data.firstname + ' ' + data.lastname);
+    // get config
+    $.post("post/getConfig").done((config) => {
+        year = config.defaultQuarter.quarter.year;
+        quarter = config.defaultQuarter.quarter.quarter;
+        $.post("post/studentProfile", { studentID: ID }).done((profile) => {
+            $.post("post/gradeCourse", { grade: profile.grade, year: year, quarter: quarter }).done((cr) => {
+                // log(cr)
+                for (let i = 0; i < profile.courseID.length; i++) {
+                    for (let j = 0; j < cr.course.length; j++) {
+                        if (profile.courseID[i] === cr.course[j].courseID) {
+                            course.push(cr.course[j]);
+                        }
+                    }
+                }
+                $.post("post/v1/listStudentHybrid", { studentID: ID, year: year, quarter: quarter }).done((hybrid) => {
+                    for (let i = 0; i < hybrid.length; i++) {
+                        fhb.push(hybrid[i]);
+                    }
+                    // log(fhb);
+                    fillTable(ID, fhb);
+                    fillButt(course);
+                })
+            })
+        })
     })
-    // add event when pick day
-    $('#datetimepicker1').on("dp.change", function (e) {
-        let inTable = [];
-        let timeLabel = [];
-        let today = $('.datePicker').data('DateTimePicker').date();
-        $.post('post/studentProfile', { studentID: cookie.monkeyWebUser }).then((cr) => {
-            for (let i = 0; i < cr.hybridDay.length; i++) {
-                let hbday = moment(cr.hybridDay[i].day);
-                if (hbday.day() === today.day()) {
-                    inTable.push("FHB: " + cr.hybridDay[i].subject);
-                    timeLabel.push(hbday.format('H:mm'));
+    // add event when pick date
+    $("#datePicker").on("dp.change", function () {
+        fillButt(course);
+    })
+    // toggle pick button
+    $(".btn-default").click(function () {
+        if (!($(this).hasClass("disabled"))) {
+            $(".btn-success").toggleClass("btn-default btn-success");
+            $(this).toggleClass("btn-default btn-success");
+        }
+    })
+    // add event when submit
+    $("#submit").click(function () {
+        if ($(".btn-success").length < 0) {
+            alert("กรุณาเลือกเวลา");
+        } else if (!($("#senderInput").val())) {
+            alert("กรุณาใส่ชื่อผู้ส่ง");
+        } else {
+            // alert("add" + $("#subjInput").val().slice(0, 5));
+            let pickDate = $('#datePicker').data('DateTimePicker').date();
+            let day = [];
+            if (pickDate.day() === 2 || pickDate.day() === 4) {
+                day.push(pickDate.hour(17).minute(0).second(0).millisecond(0).valueOf());
+            } else {
+                for (let i = 0; i < $(".btn-success").length; i++) {
+                    day.push(parseInt(pickDate.hour($($(".btn-success")[i]).attr("id").slice(3)).minute(0).second(0).millisecond(0).valueOf()));
                 }
             }
-            for (let i = 0; i < cr.courseID.length; i++) {
-                $.post('post/courseInfo', { courseID: cr.courseID[i] }).then((info) => {
-                    let crday = moment(info.day);
-                    if (crday.day() === today.day()) {
-                        inTable.push(info.courseName);
-                        timeLabel.push(crday.format('H:mm'));
-                        updateBtn(inTable, timeLabel);
-                    }
+            // log(day);
+            if (confirm("ต้องการเพิ่ม " + $("#subjInput").val() + " เวลา " + moment(day[0]).format("HH:mm") + " วันที่ " + moment(day[0]).format("DD/MM/YYYY")) == true) {
+                $.post("post/addStudentAbsenceModifier", { studentID: ID, day: day, reason: "add" + $("#subjInput").val().slice(0, 5), sender: $("#senderInput").val() }).done((data) => {
+                    location.reload();
                 })
             }
-            updateBtn(inTable, timeLabel);
-        })
-
+        }
     })
-    // btn click function
-    $(".btn").click(function () {
-        let cls = this.className;
-        if (cls.indexOf('disabled') < 0) {
-            if (cls.indexOf('btn-default') >= 0) {
-                for (let i = 0; i < 4; i++) {
-                    if ($(".btn" + (i + 1)).attr('class').indexOf('disabled') < 0 && $(".btn" + (i + 1)).attr('class').indexOf('btn-success') > 0) {
-                        $(".btn" + (i + 1)).attr('class', $(".btn" + (i + 1)).attr('class').replace('btn-success', 'btn-default'))
-                    }
-                }
-                this.className = cls.replace('btn-default', 'btn-success');
-            } else this.className = cls.replace('btn-success', 'btn-default');
-        }
-    });
-    // btn submit click
-    $("#submit").click(function () {
-        let boo = true;
-        for (let i = 0; i < 4; i++) {
-            let btn = $(".btn" + (i + 1));
-            if (btn.attr('class').indexOf('btn-success') >= 0) {
-                boo = false;
-            }
-        }
-        if (boo) {
-            alert("กรุณาเลือกวันที่และเวลา");
-        } else if ($('#contact').val().length <= 0) {
-            alert("กรุณาใส่ชื่อผู้แจ้ง")
-        } else {
-            $(".modal-body").append("<p>ต้องการเพิ่มตามนี้ใช่หรือไม่?</p>");
-            let subj = $('#subj').val().slice(0, 3) + ":" + $('#subj').val().slice(3);
-            for (let i = 0; i < 4; i++) {
-                let btn = $(".btn" + (i + 1));
-                if (btn.attr('class').indexOf('btn-success') >= 0) {
-                    $(".modal-body").append("<p>" + subj + " (" + btn.text() + ")</p>");
-                }
-            }
-            $("#confirmModal").modal();
-        }
-    });
-})
-function updateBtn(table, timeLabel) {
-    let pickday = $('.datePicker').data('DateTimePicker').date();
-    let time1 = ["8:00", "10:00", "13:00", "15:00"];
-    let time2 = ["", "", "", "17:00"];
-    let day1 = ["8:00-10:00", "10:00-12:00", "13:00-15:00", "15:00-17:00"];
-    let day2 = ["-", "-", "-", "17:00-19:00"];
-    let time = moment(0);
-    time.date(pickday.date()).month(pickday.month()).year(pickday.year());
-    // gen default button
-    if (pickday.day() === 2 || pickday.day() === 4) {
-        for (let i = 0; i < 4; i++) {
-            $(".btn" + (i + 1)).html(day2[i]);
-            if (i < 3) {
-                $(".btn" + (i + 1)).attr('class', 'btn btn-default col-xs-12 disabled btn' + (i + 1));
-                $(".btn" + (i + 1)).attr('id', '');
-            } else {
-                time.hour(time2[i].slice(0, -3)).minute('0');
-                $(".btn" + (i + 1)).attr('class', 'btn btn-default col-xs-12 btn' + (i + 1));
-                $(".btn" + (i + 1)).attr('id', time);
-            }
-            $(".modal-body").empty();
-        }
+});
+// func for fill data in button
+function fillButt(course) {
+    $(".selector").html("&nbsp;").removeClass("disabled course btn-success").addClass("btn-default");
+    let pickDate = $('#datePicker').data('DateTimePicker').date();
+    if (pickDate.day() === 2 || pickDate.day() === 4) {
+        $("#btn8").html("17-19");
+        $("#btn10").addClass("disabled");
+        $("#btn13").addClass("disabled");
+        $("#btn15").addClass("disabled");
     } else {
-        for (let i = 0; i < 4; i++) {
-            time.hour(time1[i].slice(0, -3)).minute('0');
-            $(".btn" + (i + 1)).html(day1[i]);
-            $(".btn" + (i + 1)).attr('class', 'btn btn-default col-xs-12 btn' + (i + 1));
-            $(".btn" + (i + 1)).attr('id', time);
-            $(".modal-body").empty();
-        }
-    }
-    // add cr&fhb to button
-    for (let i = 0; i < table.length; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (pickday.day() === 2 || pickday.day() === 4) {
-                if (timeLabel[i] === time2[j]) {
-                    $(".btn" + (j + 1)).html(table[i] + " (" + timeLabel[i] + ")");
-                    $(".btn" + (j + 1)).attr('class', 'btn btn-warning col-xs-12 disabled btn' + (j + 1));
-                    $(".btn" + (j + 1)).attr('id', "");
-                }
-            } else if (timeLabel[i] === time1[j]) {
-                $(".btn" + (j + 1)).html(table[i] + " (" + timeLabel[i] + ")");
-                $(".btn" + (j + 1)).attr('class', 'btn btn-warning col-xs-12 disabled btn' + (j + 1));
-                $(".btn" + (j + 1)).attr('id', "");
+        for (let i = 0; i < course.length; i++) {
+            let crDate = moment(course[i].day);
+            if (crDate.day() == pickDate.day()) {
+                $.post("post/name", { userID: course[i].tutor[0] }, function (name) {
+                    $("#btn" + crDate.hour()).html(course[i].courseName + " - " + name.nicknameEn).addClass("disabled course");
+                })
+            } else {
+                $("#btn" + crDate.hour()).html(crDate.hour() + " - " + (crDate.hour() + 2));
             }
-            $(".modal-body").empty();
         }
     }
 }
-function sendData(i) {
-    if (i < 4) {
-        let btn = $(".btn" + (i + 1));
-        if (btn.attr('class').indexOf('btn-success') >= 0) {
-            let cookie = getCookieDict();
-            let daytime = btn.attr('id');
-            $.post('post/addStudentPresenceModifier', { studentID: cookie.monkeyWebUser, day: daytime, subject: $('#subj').val(), sender: $('#contact').val() }).then((data) => {
-                sendData(i + 1);
-            })
-        } else sendData(i + 1);
-        if (i === 3) {
-            $("#confirmModal").modal("hide");
-            location.reload();
+// func for fill data in table
+function fillTable(ID, fhb) {
+    let today = moment();
+    $.post("post/listStudentAttendanceModifierByStudent", { studentID: ID, start: today.valueOf() }).done((data) => {
+        // log(data.modifier)
+        for (let i = 0; i < data.modifier.length; i++) {
+            if (data.modifier[i].reason == "addFHB:M") {
+                $("#presentTableBody").append("<tr><td class='text-center'>" + moment(data.modifier[i].day).format("ddd DD MMM HH:mm") + " MATH</td></tr>");
+            } else if (data.modifier[i].reason == "addFHB:P") {
+                $("#presentTableBody").append("<tr><td class='text-center'>" + moment(data.modifier[i].day).format("ddd DD MMM HH:mm") + " PHYSIC</td></tr>");
+            } else if (data.modifier[i].reason != "ลา" && data.modifier[i].reason != "เพิ่ม" && data.modifier[i].subject == "") {
+                for (let j = 0; j < fhb.length; j++) {
+                    if (moment(fhb[j].day).hour() == moment(data.modifier[i].day).hour() && moment(fhb[j].day).day() == moment(data.modifier[i].day).day()) {
+                        let str = (fhb[j].subject == "M") ? "MATH" : "PHYSIC";
+                        $("#absentTableBody").append("<tr><td class='text-center'>" + moment(data.modifier[i].day).format("ddd DD MMM HH:mm") + " " + str + "</td></tr>");
+                    }
+                }
+            }
         }
-    }
+    })
 }
