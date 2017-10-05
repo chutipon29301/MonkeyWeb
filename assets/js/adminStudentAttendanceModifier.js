e42c67ad564eb3cf6cde93d14fb22fb449286e18
Nuttapong <taoangle009@gmail.com>
2017-10-05 16:56:47 +0700

    add absent view for admin

--- assets/js/adminStudentAttendanceModifier.js
+++ assets/js/adminStudentAttendanceModifier.js
@@ -1,7 +1,25 @@
 const summerQ = 12;
 const summerMonth = 9;
 const summerYear = 2017;
+const crYear = 2017;
+const crQuarter = 4;
 $(document).ready(function () {
+    // for cr&fhb
+    $("#datePicker").datetimepicker({
+        format: "DD/MM/YYYY",
+        daysOfWeekDisabled: [1, 3, 5],
+        minDate: moment()
+    });
+    genCrTimePick();
+    genCrTable();
+    $("#datePicker").on("dp.change", function () {
+        genCrTimePick();
+        genCrTable();
+    });
+    $("#crTimePick").change(function () {
+        genCrTable();
+    });
+    // for summer
     genTableByName();
     $("#smByName").collapse("show");
     $("#aByName").click(function () {
@@ -23,7 +41,96 @@ $(document).ready(function () {
         genTableByWeek();
     })
 });
-// func for generate student TypeAhead(Table by Name)
+// For cr&fhb
+function genCrTimePick() {
+    $("#crTimePick").empty();
+    let pickDate = $('#datePicker').data('DateTimePicker').date();
+    if (pickDate.day() == 2 || pickDate.day() == 4) {
+        $("#crTimePick").append("<option>17-19</option>");
+    } else {
+        $("#crTimePick").append("<option>8-10</option>");
+        $("#crTimePick").append("<option>10-12</option>");
+        $("#crTimePick").append("<option>13-15</option>");
+        $("#crTimePick").append("<option>15-17</option>");
+    }
+}
+function genCrTable() {
+    $("#crPresentTable").empty();
+    $("#crAbsentTable").empty();
+    let pickDate = $('#datePicker').data('DateTimePicker').date();
+    let time = $("#crTimePick").val();
+    time = time.slice(0, time.indexOf("-"));
+    let dataDate = moment(0).year(pickDate.year()).month(pickDate.month()).date(pickDate.date()).hour(time).minute(0).second(0).millisecond(0);
+    $.post("post/listStudentAttendanceModifierByDay", { day: dataDate.valueOf() }, (data) => {
+        // log(data.absence);
+        let promise1 = [];
+        let promise2 = [];
+        for (let i in data.absence) {
+            promise1.push($.post("post/studentProfile", { studentID: data.absence[i].studentID }));
+            promise2.push($.post("post/v1/listStudentHybrid", { studentID: data.absence[i].studentID, quarter: crQuarter, year: crYear }));
+        }
+        Promise.all([
+            Promise.all(promise1),
+            Promise.all(promise2)
+        ]).then((dt) => {
+            // log(dt[0]);
+            // log(dt[1]);
+            for (let i in data.absence) {
+                if (data.absence[i].reason.slice(0, 3) == "add") {
+                    $("#crPresentTable").append(
+                        "<tr>" +
+                        "<td class='text-center'>" + data.absence[i].studentID + "</td>" +
+                        "<td class='text-center'>" + dt[0][i].nickname + " " + dt[0][i].firstname + "</td>" +
+                        "<td class='text-center'>" + data.absence[i].reason.slice(3) + "</td>" +
+                        "</tr>"
+                    )
+                } else {
+                    // log("pending");
+                    $("#crAbsentTable").append(
+                        "<tr class='" + (emergencyCheck(dataDate, moment(data.absence[i].timestamp)) ? "warning" : "") + "'>" +
+                        "<td class='text-center'>" + moment(data.absence[i].timestamp).format("DD/MM/YYYY") + "</td>" +
+                        "<td class='text-center'>" + data.absence[i].studentID + "</td>" +
+                        "<td class='text-center'>" + dt[0][i].nickname + " " + dt[0][i].firstname + "</td>" +
+                        "<td class='text-center'>" + myFHB(dt[0][i].courseID, dt[1][i], dataDate) + "</td>" +
+                        "<td class='text-center'>" + data.absence[i].reason + "</td>" +
+                        "</tr>"
+                    )
+                }
+            }
+        })
+    })
+}
+function myFHB(cr, fhb, time) {
+    // log(cr);
+    // log(fhb);
+    let promise = [];
+    for (let i in fhb) {
+        if (moment(fhb[i].day).day() == time.day() && moment(fhb[i].day).hour() == time.hour()) {
+            return ("FHB:" + fhb[i].subject);
+        }
+    }
+    for (let i in cr) {
+        promise.push($.post("post/courseInfo", { courseID: cr[i] }));
+    }
+    Promise.all(promise).then((data) => {
+        for (let i in data) {
+            if (moment(data[i].day).day() == time.day() && moment(data[i].day).hour() == time.hour()) {
+                return ("CR:" + data[i].courseName);
+            }
+        }
+    })
+}
+function emergencyCheck(pickDate, timestamp) {
+    let time = moment(0).year(pickDate.year()).month(pickDate.month()).date(pickDate.date()).hour(18);
+    let aDayValue = 24 * 60 * 60 * 1000;
+    if (pickDate.day() == 0) {
+        if (time.valueOf() - timestamp.valueOf() < (2 * aDayValue)) return true;
+        else return false;
+    } else if (time.valueOf() - timestamp.valueOf() < aDayValue) {
+        return true;
+    } else return false;
+}
+// For summer
 function genTableByName() {
     let allStudent = [];
     // gen typeahead input
@@ -95,7 +202,6 @@ function genTableByName() {
         log("=>fail to post/allStudent");
     })
 }
-// func for generate absent table by picked student name
 function genPickedTable(ID) {
     let time = moment();
     $.post("post/listStudentAttendanceModifierByStudent", { studentID: ID, start: time.valueOf() }).done(function (data) {
@@ -135,7 +241,6 @@ function genPickedTable(ID) {
         log("=>fail to post/listStudentAttendanceModifierByStudent");
     })
 }
-// func for generate table by week
 function genTableByWeek() {
     $("#smWeekBody").empty();
     const startDay = parseInt($("#weekSelect").val());
