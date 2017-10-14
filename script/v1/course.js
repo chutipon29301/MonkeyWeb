acdc1edd4e1925e2b9219aefb3a78a2de2e0cf15
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-10-14 11:45:55 +0700

    Add allCourse post method for v1

--- script/v1/course.js
+++ script/v1/course.js
@@ -1,13 +1,12 @@
-module.exports = function (app, db, post, gradeBitToString) {
+module.exports = function (app, db, post, gradeBitToString, gradeBitToArray) {
     var courseDB = db.collection('course');
     var configDB = db.collection('config');
+    var userDB = db.collection('user');
 
     post('/post/v1/allCourse', function (req, res) {
         configDB.findOne({
             _id: 'config'
         }).then(config => {
-            var response;
-            console.log(config);
             var quarter, year;
             if (req.body.year === undefined || req.body.quarter === undefined) {
                 quarter = config.defaultQuarter.quarter.quarter;
@@ -16,14 +15,32 @@ module.exports = function (app, db, post, gradeBitToString) {
                 quarter = parseInt(req.body.quarter);
                 year = parseInt(req.body.year);
             }
-            courseDB.find({
+            return courseDB.find({
                 year: year,
                 quarter: quarter
-            }).toArray().then(data => {
-                var promise = [];
-                console.log(data);
-                res.status(200).send('OK');
-            });
+            }).toArray();
+        }).then(allCourse => {
+            var promise = [];
+            for (let i = 0; i < allCourse.length; i++) {
+                promise.push(userDB.findOne({
+                    _id: parseInt(allCourse[i].tutor[0])
+                }));
+            }
+            Promise.all(promise).then(values => {
+                var response = [];
+                for (let i = 0; i < values.length; i++) {
+                    response.push({
+                        courseID: allCourse[i]._id,
+                        courseName: allCourse[i].subject + gradeBitToString(allCourse[i].grade) + allCourse[i].level,
+                        day: allCourse[i].day,
+                        tutorName: values[i].nicknameEn,
+                        studentCount: allCourse[i].student.length,
+                        room: allCourse[i].room,
+                        grade: gradeBitToArray(allCourse[i].grade)
+                    });
+                }
+                res.status(200).send(response);
+            })
         });
     });
 }
