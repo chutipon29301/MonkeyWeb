fe0445aaf21d9667110a9b887d0e998c3421c449
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-10-21 09:43:23 +0700

    Add upload document method and load document method

--- script/v1/document.js
+++ script/v1/document.js
@@ -17,27 +17,51 @@ module.exports = function (app, db, post) {
             _id: 'config'
         }).then(config => {
             var files = req.files;
+            var error;
+            var date;
+            if (req.body.displayDate !== undefined) {
+                date = new Date(parseInt(req.body.date));
+            } else {
+                date = new Date();
+            }
             var copyFile = (courseID, files, res) => {
                 var dir = config.documentPath + courseID;
                 try {
                     fs.ensureDirSync(dir);
                 } catch (err) {
                     return res.status(500).send({
-                        err: 1,
+                        err: err.errno,
                         msg: err
                     });
                 }
                 for (let i = 0; i < files.length; i++) {
+                    var path = config.documentPath.substring(0, config.documentPath.length - 1);
                     try {
                         fs.moveSync(files[i].path, dir + '/' + files[i].originalname);
-                    } catch (err) {
-                        return res.status(520).send({
-                            err: err.errno,
-                            msg: err
+                        studentDocumentDB.insertOne({
+                            name: files[i].originalname.substring(0, files[i].originalname.indexOf('.')),
+                            location: path.substring(path.lastIndexOf('/'), path.length) + '/' + config.documentPath.substring(0, config.documentPath.length - 1).substring(config.documentPath.lastIndexOf('/'), config.documentPath.length) + courseID + '/' + files[i].originalname,
+                            fileType: files[i].mimetype.substring(files[i].mimetype.indexOf('/') + 1, files[i].mimetype.length),
+                            courseID: courseID,
+                            upload: new Date(),
+                            displayDate: date
+                        }, function (err, result) {
                         });
+                    } catch (err) {
+                        if (error === undefined) {
+                            error = [];
+                        }
+                        error.push(err);
                     }
                 }
-                return res.status(200).send('OK');
+                if (error) {
+                    return res.status(500).send({
+                        err: 1,
+                        msg: error
+                    });
+                } else {
+                    return res.status(200).send('OK');
+                }
             }
             if (req.body.courseID !== undefined) {
                 courseDB.findOne({
@@ -52,7 +76,41 @@ module.exports = function (app, db, post) {
                     copyFile(course._id, files, res);
                 })
             } else {
-                copyFile('' + config.defaultQuarter.quarter.year + config.defaultQuarter.quarter.quarter, files, res);
+                copyFile(config.defaultQuarter.quarter.year + (((('' + config.defaultQuarter.quarter.quarter).length) === 1) ? '0' : '') + config.defaultQuarter.quarter.quarter, files, res);
+            }
+        });
+    });
+
+    post('/post/v1/listDocument', function (req, res) {
+        configDB.findOne({
+            _id: 'config'
+        }).then(config => {
+            var allQuarterDocument = () => studentDocumentDB.find({
+                courseID: config.defaultQuarter.quarter.year + (((('' + config.defaultQuarter.quarter.quarter).length) === 1) ? '0' : '') + config.defaultQuarter.quarter.quarter
+            }).toArray();
+
+            var courseDocument = (courseID) => studentDocumentDB.find({
+                courseID: courseID
+            }).toArray();
+
+            if (req.body.courseID === undefined && req.body.studentID === undefined) {
+                allQuarterDocument().then(data => {
+                    for (let i = 0; i < data.length; i++) {
+                        data[i].documentID = data[i]._id;
+                        delete data[i]._id;
+                    }
+                    res.status(200).send(data);
+                });
+            } else if (req.body.courseID !== undefined && req.body.studentID === undefined) {
+                courseDocument(req.body.courseID).then(data => {
+                    for (let i = 0; i < data.length; i++) {
+                        data[i].documentID = data[i]._id;
+                        delete data[i]._id;
+                    }
+                    res.status(200).send(data);
+                });
+            }else{
+                res.status(200).send('OK');
             }
         });
     });
