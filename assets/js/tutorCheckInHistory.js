766b430d120f9ccd2fe1f8a42f4e3292f03c0b99
Nuttapong <taoangle009@gmail.com>
2017-11-19 15:36:26 +0700

    commit for pull

--- assets/js/tutorCheckInHistory.js
+++ assets/js/tutorCheckInHistory.js
@@ -1,4 +1,10 @@
 let cookie = getCookieDict();
 log(cookie.monkeyWebUser);
-let now = moment();
-log(now.format("DD/MM/YYYY"));
+let start = moment(0).day(1).month(10).year(2017).hour(0);
+logMoment(start);
+$.post("post/v1/listInterval").then(data => {
+    log(data)
+})
+function logMoment(moment) {
+    log(moment.format("DD/MM/YYYY HH:mm:ss"));
+}
