fc395f6b52a6039b0b32c7a6d3bc385fc7e6c3a3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-11-19 16:31:14 +0700

    Add method for update subRegistrationState

--- script/v1/student.js
+++ script/v1/student.js
@@ -13,7 +13,7 @@ module.exports = function (app, db, post) {
     const ABSENT = 1;
     const PRESENT = 2;
 
-    post('post/v1/addAbsent', function (req, res) {
+    post('post/v1/addStudentAbsent', function (req, res) {
         if (!req.body.userID || (!req.body.courseID && !req.body.hybridID) || !req.body.date) {
             return res.status(400).send({
                 err: -1,
@@ -32,4 +32,39 @@ module.exports = function (app, db, post) {
 
         }
     });
+
+    post('/post/v1/updateStudentRegistrationState', function (req, res) {
+        if (!(req.body.studentID && req.body.quarter && req.body.year && req.body.registrationState && req.body.subRegistrationState)) {
+            return res.status(400).send({
+                err: -1,
+                msg: 'Bad Request'
+            });
+        }
+        userDB.findOne({
+            _id: parseInt(req.body.studentID)
+        }).then(data => {
+            let index = data.student.quarter.findIndex(x => x.year === parseInt(req.body.year) && x.quarter === parseInt(req.body.quarter));
+            if (index === -1) {
+                return res.status(404).send({
+                    err: 404,
+                    msg: 'Year or quarter not found'
+                });
+            }
+            var stateObject = data.student.quarter;
+            stateObject[index].registrationState = req.body.registrationState;
+            stateObject[index].subRegistrationState = req.body.subRegistrationState;
+            console.log(data.student.quarter);
+            console.log(stateObject);
+            userDB.updateOne({
+                _id: parseInt(req.body.studentID)
+            }, {
+                $set: {
+                    'student.quarter': stateObject
+                }
+            }).then(result => {
+                return res.status(200).send('OK');
+            });
+        });
+    });
+
 }
