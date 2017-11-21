9871871c19838572918a8653f05ff0b0129986e6
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-11-21 19:49:12 +0700

    Add student attendance method (not done)

--- script/v1/student.js
+++ script/v1/student.js
@@ -14,7 +14,7 @@ module.exports = function (app, db, post) {
     const PRESENT = 2;
 
     post('post/v1/addStudentAbsent', function (req, res) {
-        if (!req.body.userID || (!req.body.courseID && !req.body.hybridID) || !req.body.date) {
+        if (!(req.body.userID && req.body.date && (req.body.courseID || req.body.hybridID) && req.body.reason && req.body.sender)) {
             return res.status(400).send({
                 err: -1,
                 msg: 'Bad Request'
@@ -22,13 +22,92 @@ module.exports = function (app, db, post) {
         }
         if (req.body.courseID) {
             attendanceDB.insertOne({
+                timestamp: new Date(),
                 userID: parseInt(req.body.userID),
                 courseID: req.body.courseID,
                 hybridID: NONE,
                 date: req.body.date,
-                type: ABSENT
+                type: ABSENT,
+                reason: req.body.reason,
+                sender: req.body.sender
             });
         } else if (req.body.hybridID) {
+            attendanceDB.insertOne({
+                timestamp: new Date(),
+                userID: parseInt(req.body.userID),
+                courseID: NONE,
+                hybridID: req.body.hybridID,
+                date: req.body.date,
+                type: ABSENT,
+                reason: req.body.reason,
+                sender: req.body.sender
+            });
+        }
+        res.status(200).send('OK');
+    });
+
+    post('/post/v1/addStudentPresent', function (req, res) {
+        if (!(req.body.userID && req.body.date && (req.body.courseID || req.body.hybridID) && req.body.sender)) {
+            return res.status(400).send({
+                err: -1,
+                msg: 'Bad Request'
+            });
+        }
+        if (req.body.courseID) {
+            attendanceDB.insertOne({
+                timestamp: new Date(),
+                userID: parseInt(req.body.userID),
+                courseID: req.body.courseID,
+                hybridID: NONE,
+                date: req.body.date,
+                type: PRESENT,
+                sender: req.body.sender
+            });
+        } else if (req.body.hybridID) {
+            attendanceDB.insertOne({
+                timestamp: new Date(),
+                userID: parseInt(req.body.userID),
+                courseID: NONE,
+                hybridID: req.body.hybridID,
+                date: req.body.date,
+                type: PRESENT,
+                sender: req.body.sender
+            });
+        }
+        res.status(200).send('OK');
+    });
+
+    post('/post/v1/listAttendance', function (req, res) {
+        if (!((req.body.startDate && req.body.endDate) || req.body.date || (req.body.studentStartDate && req.body.studentEndDate && req.body.studentID))) {
+            return res.status(400).send({
+                err: -1,
+                msg: 'Bad Request'
+            });
+        }
+
+        if (req.body.startDate && req.body.endDate) {
+            attendanceDB.find({
+                timestamp: {
+                    $gte: new Date(parseInt(req.body.startDate)),
+                    $lte: new Date(parseInt(req.body.endDate))
+                }
+            }, {
+                sort: {
+                    timestamp: -1
+                }
+            }).toArray().then(result => {
+                for (let i = 0; i < result.lenght; i++) {
+                    result[i].timestamp = new Date(result[i].timestamp).valueOf();
+                    result[i].attendanceID = result[i]._id;
+                    result[i].date = new Date(result[i].date).valueOf();
+                    delete result[i]._id;
+                }
+            });
+        } else if (req.body.date) {
+            // attendanceDB.find({
+
+            // })
+        } else if (req.body.studentStartDate && req.body.studentEndDate && req.body.studentID) {
 
         }
     });
@@ -65,12 +144,12 @@ module.exports = function (app, db, post) {
         });
     });
 
-    post('/post/v1/getRegistrationState', function(req,res){
-        if(!(req.body.studentID && req.body.quarter && req.body.year)){
+    post('/post/v1/getRegistrationState', function (req, res) {
+        if (!(req.body.studentID && req.body.quarter && req.body.year)) {
             return res.status(400).send({
                 err: -1,
                 msg: 'Bad Request'
-            });   
+            });
         }
 
         userDB.findOne({
