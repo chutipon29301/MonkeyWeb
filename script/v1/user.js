a6a7e7c18c99075d794264059cd049d96b7ad707
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-01-28 15:23:20 +0700

    Fix method for app

--- script/v1/user.js
+++ script/v1/user.js
@@ -1,23 +1,36 @@
 var ObjectID = require("mongodb").ObjectID;
 
-module.exports = function (app, db, post ,CryptoJS) {
+module.exports = function (app, db, post, CryptoJS) {
 
     var userDB = db.collection('user');
     var User = db.collection("user");
-    post('/post/v1/signup',function(req,res,next){
-        User.findOne({_id:parseInt(req.body.id)},function(err,user){
-            if(err) throw err;
-            if(!user){
-                let pwd = (Math.floor(Math.random()*10000))+'';
-                User.insertOne({_id:parseInt(req.body.id),password:CryptoJS.SHA3(pwd).toString()},function(err){
-                    if(err) throw err;
-                    res.status(200).send({id : parseInt(req.body.id) , password : pwd})
-                })
-            }else{
-                res.status(200).send({err : 202 , msg : 'This id has been used.'})
+
+    post('/post/v1/signup', function (req, res, next) {
+        User.findOne({
+            _id: parseInt(req.body.id)
+        }, function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                let pwd = (Math.floor(Math.random() * 10000)) + '';
+                User.insertOne({
+                    _id: parseInt(req.body.id),
+                    password: CryptoJS.SHA3(pwd).toString()
+                }, function (err) {
+                    if (err) throw err;
+                    res.status(200).send({
+                        id: parseInt(req.body.id),
+                        password: pwd
+                    })
+                });
+            } else {
+                res.status(200).send({
+                    err: 202,
+                    msg: 'This id has been used.'
+                });
             }
-        })
-    })
+        });
+    });
+
     post('/post/v1/listTutor', function (req, res) {
         userDB.find({
             position: {
@@ -30,11 +43,36 @@ module.exports = function (app, db, post ,CryptoJS) {
                 delete result[i].password;
                 delete result[i].firstnameEn;
                 delete result[i].lastnameEn;
-                // delete result[i].nicknameEn;
                 delete result[i].phone;
                 delete result[i].tutor;
             }
             res.status(200).send(result);
+        });
+    });
+
+    post('/post/v1/listTutorJson', function (req, res) {
+        userDB.find({
+            position: {
+                $in: ['tutor', 'admin', 'dev']
+            },
+            'tutor.status': 'active'
+        },{
+            sort: {
+                _id: -1
+            }
+        }).toArray().then(result => {
+            for (let i = 0; i < result.length; i++) {
+                result[i].tutorID = result[i]._id;
+                delete result[i]._id;
+                delete result[i].password;
+                delete result[i].firstnameEn;
+                delete result[i].lastnameEn;
+                delete result[i].phone;
+                delete result[i].tutor;
+            }
+            res.status(200).send({
+                tutors: result.reverse()
+            });
         });
     });
 
