22dc5532edc25c1ba7a254bbfe14bd803dbf1092
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-02-04 10:21:50 +0700

    Add dev page

--- script/dev/task.js
+++ script/dev/task.js
@@ -1,8 +1,32 @@
+var ObjectID = require("mongodb").ObjectID;
+
 module.exports = function (app, passport, db) {
     app.get('/taskDB', passport.isLoggedIn, (req, res) => {
-        db.collection('task').find({}).toArray().then(tasks => {
-            res.render('taskDB',{
+        if(req.user.position !== 'dev') return res.render('404');
+        db.collection('task').find({
+            parent: null
+        }).sort({
+            createOn: -1
+        }).toArray().then(tasks => {
+            res.render('taskDB', {
                 tasks: tasks
+            });
+        });
+    });
+
+    app.get('/taskDetail', passport.isLoggedIn, (req, res) => {
+        if(req.user.position !== 'dev') return res.render('404');
+        Promise.all([
+            db.collection('task').findOne({
+                _id: ObjectID(req.query.id)
+            }),
+            db.collection('task').find({
+                ancestors: ObjectID(req.query.id)
+            }).toArray()
+        ]).then(values => {
+            res.render('taskDetail', {
+                head: values[0],
+                tails: values[1]
             });
         });
     });
