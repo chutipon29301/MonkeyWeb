1811a3ce45139f5ecc2191f2ec9d04fb69d779de
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-02-04 10:42:14 +0700

    Add module and remove unused file

--- script/dev/index.js
+++ script/dev/index.js
@@ -2,9 +2,11 @@ module.exports = function (passport, db) {
     var bodyParser = require('body-parser');
     var cookieParser = require('cookie-parser');
     var express = require('express');
+    var morgan = require('morgan');
     var app = express();
 
     app.use(express.static('assets'));
+    app.use(morgan('tiny'))
 
     app.get('/', passport.isLoggedIn, (req, res) => {
         res.render('devHome');
