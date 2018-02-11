6b90d3d78b3fa16f6d32442836d00fc2916a843a
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-02-11 11:17:25 +0700

    Prepare for v2

--- webFlow.js
+++ webFlow.js
@@ -71,6 +71,7 @@ module.exports = function (app, db, pasport) {
     };
 
     app.use('/dev', require('./script/dev/index')(auth, db));
+    app.use('/v2', require('./script/v2/index')(auth, db));
     app.get('*', function (req, res, next) {
         if (req.url == '/login') req.logOut();
         if (req.isAuthenticated()) {
