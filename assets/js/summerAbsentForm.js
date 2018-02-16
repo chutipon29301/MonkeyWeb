3cbf178c1cacb9c1f7faa2a8ea9fd86cb0578c0a
Nuttapong <taoangle009@gmail.com>
2018-02-16 10:15:16 +0700

    Finished summer absent form

--- assets/js/summerAbsentForm.js
+++ assets/js/summerAbsentForm.js
@@ -10,11 +10,18 @@ async function getCourse() {
     for (let i in allQ.quarter) {
         if (allQ.quarter[i].year === year && allQ.quarter[i].quarter === quarter) {
             $("#absentHeader").html("แบบฟอร์มลา " + allQ.quarter[i].name);
+            $("#presentHeader").html("แบบฟอร์มชดเชย " + allQ.quarter[i].name);
         }
     }
-    let stdTable = await $.post("post/v1/studentTimeTable", { year: year, quarter: quarter, studentID: cookies.monkeyWebUser });
+    let startT = moment(0).year(2018).month(2).date(12).hour(6);
+    let endT = moment(0).year(2018).month(2).date(30).hour(23);
+    let [stdTable, history] = await Promise.all([
+        $.post("post/v1/studentTimeTable", { year: year, quarter: quarter, studentID: cookies.monkeyWebUser }),
+        $.post("post/v1/listAttendance", { studentStartDate: startT.valueOf(), studentEndDate: endT.valueOf(), studentID: cookies.monkeyWebUser })
+    ]);
     timeTable = stdTable;
     enableBtn(stdTable);
+    fillHistory(history);
 }
 function enableBtn(table) {
     for (let i in table.course) {
@@ -22,48 +29,101 @@ function enableBtn(table) {
         $(".btn-" + t.hour()).removeClass("disabled");
     }
 }
+function fillHistory(history) {
+    for (let i in history) {
+        if (history[i].type === 1) {
+            if (history[i].reason === 'SummerAbsent') {
+                let t = moment(history[i].date);
+                $('#a' + t.format('DD-HH')).addClass('btn-danger AID' + history[i]._id).removeClass('btn-light');
+            }
+        } else if (history[i].type === 2) {
+            for (let j in timeTable.course) {
+                if (history[i].courseID === timeTable.course[j].courseID) {
+                    let t = moment(history[i].date);
+                    $('#p' + t.format('DD-HH')).addClass('btn-info AID' + history[i]._id).removeClass('btn-light').html(history[i].courseName);
+                }
+            }
+        }
+    }
+}
 $(".btn-a").click(function () {
-    if (!$(this).hasClass("disabled")) {
-        $(this).toggleClass("btn-light btn-danger");
+    if ($(this).hasClass("btn-danger")) {
+        if (confirm('ต้องการลบประวัติการลานี้?')) {
+            let str = this.className.slice(this.className.indexOf('AID') + 3, this.className.indexOf('AID') + 27);
+            removeHistory(str);
+        }
+    } else if ($(this).hasClass("btn-light")) {
+        if (!$(this).hasClass('disabled')) {
+            let t = moment(0).year(2018).month(2).date(parseInt(this.id.slice(1, 3))).hour(parseInt(this.id.slice(4)));
+            let crName;
+            let crID;
+            for (let i in timeTable.course) {
+                let newt = moment(timeTable.course[i].day);
+                if (newt.hour() === t.hour()) {
+                    crName = timeTable.course[i].courseName;
+                    crID = timeTable.course[i].courseID;
+                }
+            }
+            if (confirm('ต้องการลาคอร์ส ' + crName + '?')) {
+                addAbsent(crID, t);
+            }
+        }
     }
 });
 $(".btn-p").click(function () {
-    if (!$(this).hasClass("disabled")) {
-        $(this).toggleClass("btn-light btn-info");
-    }
-});
-$("#submit").click(function () {
-    if ($(".btn-danger").length > 0) {
-        if ($("#sender").val().length > 0) {
-            sendData();
-        } else {
-            alert("กรุณาใส่ชื่อผู้แจ้ง");
+    if ($(this).hasClass("btn-light")) {
+        let t = moment(0).year(2018).month(2).date(parseInt(this.id.slice(1, 3))).hour(parseInt(this.id.slice(4)));
+        for (let i in timeTable.course) {
+            $("#presentSubj").append(
+                "<option value=" + t.date() + "-" + timeTable.course[i].courseID + ">" + timeTable.course[i].courseName + "</option>"
+            );
         }
-    } else {
-        self.location = "/studentProfile";
+        $("#addPresentModal").modal('show');
+    } else if ($(this).hasClass("btn-info")) {
+        if (confirm('ต้องการลบประวัติการชดเชยนี้?')) {
+            let str = this.className.slice(this.className.indexOf('AID') + 3, this.className.indexOf('AID') + 27);
+            removeHistory(str);
+        }
     }
 });
-async function sendData() {
+async function removeHistory(id) {
+    $("#waiting").modal('show');
+    let cb = await $.post('post/v1/deleteAttendance', { attendanceID: id });
+    log(cb);
+    location.reload();
+}
+async function addAbsent(crID, time) {
+    $("#waiting").modal('show');
     let body = {};
     let cookies = getCookieDict();
-    let promise = [];
     body.userID = cookies.monkeyWebUser;
     body.reason = "SummerAbsent";
-    body.sender = $("#sender").val();
-    for (let i = 0; i < $(".btn-danger").length; i++) {
-        let str = $(".btn-danger")[i].id;
-        let t = moment(0);
-        t.date(parseInt(str.slice(1, 3))).month(2).year(2018).hour(parseInt(str.slice(-2)));
-        body.date = t.valueOf();
-        for (let j in timeTable.course) {
-            let time = moment(timeTable.course[j].day);
-            if (time.hour() === t.hour()) {
-                body.courseID = timeTable.course[j].courseID;
-            }
-        }
-        promise.push($.post("post/v1/addStudentAbsent", body));
-    }
-    let cb = await Promise.all(promise);
+    body.sender = 'ผปค.';
+    body.courseID = crID;
+    body.date = time.valueOf();
+    let cb = await $.post("post/v1/addStudentAbsent", body);
     log(cb);
-    self.location = "/registrationReceipt"
+    location.reload();
+}
+$("#addPresentBtn").click(function () {
+    let str = $("#presentSubj").val();
+    let date = str.slice(0, str.indexOf('-'));
+    let id = str.slice(str.indexOf('-') + 1);
+    if (confirm('ยืนยันการชดเชย?')) {
+        addPresent(id, date);
+    }
+});
+async function addPresent(crID, date) {
+    $("#addPresentModal").modal('hide');
+    $("#waiting").modal('show');
+    let t = moment(0).year(2018).month(2).date(parseInt(date)).hour(15);
+    let body = {};
+    let cookies = getCookieDict();
+    body.userID = cookies.monkeyWebUser;
+    body.date = t.valueOf();
+    body.courseID = crID;
+    body.sender = 'ผปค.';
+    let cb = await $.post('post/v1/addStudentPresent', body);
+    log(cb);
+    location.reload();
 }
