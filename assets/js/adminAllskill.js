0d56009a97369ff8135a4b1907a0678b40b51f4d
Nuttapong <taoangle009@gmail.com>
2018-03-27 13:20:44 +0700

    add student redirect in skillInfo

--- assets/js/adminAllskill.js
+++ assets/js/adminAllskill.js
@@ -7,3 +7,24 @@ function gotoSkillInfoPage(ID) {
     writeCookie('monkeySelectedSkill', ID);
     self.location = '/adminSkillInfo';
 }
+
+$("#subject").change(function () {
+    if (this.value === "ME") {
+        $(".Mskill").show();
+        $(".Eskill").show();
+    } else if (this.value === "M") {
+        $(".Mskill").show();
+        $(".Eskill").hide();
+    } else if (this.value === "E") {
+        $(".Mskill").hide();
+        $(".Eskill").show();
+    }
+});
+
+// $("#addBtn").click(async function () {
+//     if (confirm("Are you sure to add new skill?")) {
+//         let body = {};
+//         body.year = $("#quarter-select").val().slice(0, 4);
+//         body.quarter = $("#quarter-select").val().slice(5);
+//     }
+// });
