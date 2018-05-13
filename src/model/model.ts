7143ab698ef7ab89ccce98040c99672b5ad63cf9
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-05-13 11:54:14 +0700

    Add tslint for coding style

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,21 +1,20 @@
-import { Connection, Request, ConnectionError } from "tedious";
-import { connect } from "./SQLconnection";
-import { model as user } from './v1/user'
-import { assignmentExpression } from "babel-types";
+import { Request } from 'tedious';
+import { connect } from './SQLconnection';
+import { model as user } from './v1/user';
 
-let model = {
+const model = {
     ...user,
-    query:function(q:string){
-        connect((err,connection)=>{
-            let request = new Request(q,(err,rowCount)=>{
-                if(err) throw err;
+    query: (q: string) => {
+        connect((err, connection) => {
+            const request = new Request(q, (err, rowCount) => {
+                if (err) { throw err; }
             });
-            request.on('row',(col)=>{
-                console.log(col.map((e:any)=>e.value))
-            })
+            request.on('row', (col) => {
+                console.log(col.map((e: any) => e.value));
+            });
             connection.execSql(request);
         });
-    }
-}
+    },
+};
 
 export default model;
