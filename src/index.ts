c6f6c127bce6bba0399d556b0aa1993ae32df08a
Pepe <skpepe134@gmail.com>
2018-05-19 08:08:18 +0700

    before pull

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import * as fs from 'fs-extra';
 import * as http from 'http';
 import * as https from 'https';
 import * as logger from 'morgan';
+import * as passport from 'passport';
+import * as validator from 'express-validator';
 import { join } from 'path';
 import Controller from './controllers/Controller';
 import model from './model/model';
@@ -20,6 +22,12 @@ app.use(express.static(join(__dirname, '../public')));
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
+app.use(cookieParser(process.env.COOKIE_SECRET || 'TEST'));
+app.use(validator());
+app.use(logger('dev'));
+app.use(passport.initialize());
+app.use(passport.session());
+
 const caPath = join(__dirname, '../MonkeyWebConfig/ca_bundle.crt');
 const keyPath = join(__dirname, '../MonkeyWebConfig/private.key');
 const certPath = join(__dirname, '../MonkeyWebConfig/certificate.crt');
@@ -35,8 +43,6 @@ if (fs.existsSync(caPath) && fs.existsSync(keyPath) && fs.existsSync(certPath)) 
     })).listen(80);
 }
 
-app.use(cookieParser(process.env.COOKIE_SECRET || 'TEST'));
-app.use(logger('dev'));
 
 const controller = new Controller(app);
 
