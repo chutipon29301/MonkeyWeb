feb2e8c93862e3b2357cae043f53d4e2b9e5d6c1
Pepe <skpepe134@gmail.com>
2018-05-20 16:48:59 +0700

    finish login method

--- src/controllers/api/v1/index.ts
+++ src/controllers/api/v1/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
+import { body } from 'express-validator/check';
+import * as jwt from 'jwt-simple';
+import { getUserLogin } from '../../../model/v1/user';
 import { router as course } from './class';
 import { router as quarter } from './quarter';
 import { router as room } from './room';
 import { router as user } from './user';
-import { body } from 'express-validator/check';
-import { getUserInfo } from '../../../model/v1/user';
-import * as jwt from 'jwt-simple';
 
 export const router = Router();
 
@@ -15,23 +15,25 @@ router.use('/room', room);
 router.use('/user', user);
 
 router.post('/login',
-    body('userID').isNumeric(),
+    body('userID').exists(),
     body('password').exists(),
     (req, res) => {
-        getUserInfo(req.body.userID).subscribe(
-            (user)=>{
-                //TODO: wait for user.password to check
-
-
-                let expire = new Date();
-                expire.setDate(expire.getDate()+7);
-                let token = 'bearer '+jwt.encode({
-                    userID : user.ID,
-                    expire
-                },process.env.JWT_SECRET);
-                return res.status(200).send({ token , expire })
+        console.log(req.body)
+        getUserLogin(Number(req.body.userID), req.body.password).subscribe(
+            (user) => {
+                if (user) {
+                    let expire = new Date();
+                    expire.setDate(expire.getDate() + 7);
+                    let token = 'bearer ' + jwt.encode({
+                        userID: req.body.userID,
+                        expire
+                    }, process.env.JWT_SECRET);
+                    return res.status(200).send({ token, expire });
+                } else {
+                    return res.sendStatus(400);
+                }
             },
-            (err)=>{
+            (err) => {
                 return res.status(400).send({ err });
             }
         )
