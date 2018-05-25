e058092300c9935e8c68e6878018f1f390c73ad2
Nuttapong <taoangle009@gmail.com>
2018-05-25 09:58:01 +0700

    remove old component

--- client/monkeyweb/src/app/app.module.ts
+++ client/monkeyweb/src/app/app.module.ts
@@ -17,17 +17,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    DashboardComponent,
-    NavBarComponent,
   ],
   imports: [
     BrowserModule,
