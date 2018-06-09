444fc71167453337e24408177a8891063fdecb8d
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-06-10 02:56:33 +0700

    Modify class list

--- src/repositories/v1/Class.ts
+++ src/repositories/v1/Class.ts
@@ -169,21 +169,21 @@ export class Class {
     ): Observable<ClassList[]> {
         let replacements: Partial<IClassModel> = { QuarterID };
         let statement =
-            'SELECT Class.ClassName, Class.ClassDate, Class.Grade, Room.RoomName, Users.NicknameEn, ( ' +
-            '   SELECT COUNT(*) ' +
-            '   FROM ClassRegistration ' +
-            '       LEFT JOIN Users ON ClassRegistration.StudentID = Users.ID ' +
-            '       LEFT JOIN StudentState ON Users.ID = StudentState.StudentID ' +
-            '   WHERE ClassRegistration.ClassID = Class.ID AND Users.UserStatus <> \'terminated\' AND StudentState.Stage <> \'dropped\' AND StudentState.QuarterID = Class.QuarterID ' +
-            ') AS StudentCount ' +
-            'FROM Class ' +
-            '   LEFT JOIN Room ON Class.RoomID = Room.ID ' +
-            '   LEFT JOIN Users ON Class.TutorID = Users.ID ' +
-            'WHERE Class.QuarterID = :QuarterID ';
-        // if (ClassType) {
-        //     statement += 'AND Class.ClassType = :ClassType';
-        //     replacements = { ...replacements, ClassType };
-        // }
+            `SELECT Class.ID Class.ClassName, Class.ClassDate, Class.Grade, Room.RoomName, Users.NicknameEn, (
+                SELECT COUNT(*)
+                FROM ClassRegistration
+                    LEFT JOIN Users ON ClassRegistration.StudentID = Users.ID
+                    LEFT JOIN StudentState ON Users.ID = StudentState.StudentID
+               WHERE ClassRegistration.ClassID = Class.ID AND Users.UserStatus <> \'terminated\' AND StudentState.Stage <> \'dropped\' AND StudentState.QuarterID = Class.QuarterID
+            ) AS StudentCount
+            FROM Class
+                LEFT JOIN Room ON Class.RoomID = Room.ID
+                LEFT JOIN Users ON Class.TutorID = Users.ID
+            WHERE Class.QuarterID = :QuarterID`;
+        if (ClassType) {
+            statement += 'AND Class.ClassType = :ClassType';
+            replacements = { ...replacements, ClassType };
+        }
         return Connection.getInstance().query<ClassList>(statement, {
             raw: true,
             replacements,
@@ -195,10 +195,10 @@ export class Class {
         ID: number,
     ): Observable<ClassInfo> {
         const statement =
-            'SELECT Class.ClassName, Class.ClassDate, Class.ClassDescription, Class.TutorID, Class.RoomID, Class.Grade, Class.ClassTimes, Class.ClassType, Users.FirstnameEn, Users.LastnameEn, Users.NicknameEn ' +
-            'FROM Class ' +
-            '   JOIN Users ON Users.ID = Class.TutorID ' +
-            'WHERE Class.ID = :ID';
+            `SELECT Class.ClassName, Class.ClassDate, Class.ClassDescription, Class.TutorID, Class.RoomID, Class.Grade, Class.ClassTimes, Class.ClassType, Users.FirstnameEn, Users.LastnameEn, Users.NicknameEn
+            FROM Class
+                JOIN Users ON Users.ID = Class.TutorID
+            WHERE Class.ID = :ID`;
         return Connection.getInstance().query<ClassInfo>(statement, {
             raw: true,
             replacements: { ID },
@@ -213,22 +213,22 @@ export class Class {
         ID: number,
     ): Observable<StudentInClass[]> {
         const statement =
-            'SELECT Users.ID, StudentState.Grade, Users.Nickname, Users.Firstname, Users.Lastname, ( ' +
-            '   SELECT COUNT(*) ' +
-            '   FROM ClassRegistration ' +
-            '       JOIN Class course ON course.ID = ClassRegistration.ClassID ' +
-            '   WHERE ClassRegistration.StudentID = Users.ID AND course.QuarterID = Class.QuarterID AND course.ClassType = \'Course\' ' +
-            ') AS CountCourse, ( ' +
-            '   SELECT COUNT(*) ' +
-            '   FROM ClassRegistration ' +
-            '       JOIN Class course ON course.ID = ClassRegistration.ClassID ' +
-            '   WHERE ClassRegistration.StudentID = Users.ID AND course.QuarterID = Class.QuarterID AND course.ClassType = \'Hybrid\' ' +
-            ') AS CountHybrid ' +
-            'FROM Class ' +
-            '   JOIN ClassRegistration ON ClassRegistration.ClassID = Class.ID ' +
-            '   JOIN Users ON ClassRegistration.StudentID = Users.ID ' +
-            '   JOIN StudentState ON Class.QuarterID = StudentState.QuarterID AND Users.ID = StudentState.StudentID ' +
-            'WHERE StudentState.Stage <> \'dropped\' AND Users.UserStatus <> \'terminated\' AND Class.ID = :ID';
+            `SELECT Users.ID, StudentState.Grade, Users.Nickname, Users.Firstname, Users.Lastname, (
+                SELECT COUNT(*)
+                FROM ClassRegistration
+                    JOIN Class course ON course.ID = ClassRegistration.ClassID
+                WHERE ClassRegistration.StudentID = Users.ID AND course.QuarterID = Class.QuarterID AND course.ClassType = \'Course\'
+            ) AS CountCourse, (
+                SELECT COUNT(*)
+                FROM ClassRegistration
+                    JOIN Class course ON course.ID = ClassRegistration.ClassID
+               WHERE ClassRegistration.StudentID = Users.ID AND course.QuarterID = Class.QuarterID AND course.ClassType = \'Hybrid\'
+            ) AS CountHybrid
+            FROM Class
+                JOIN ClassRegistration ON ClassRegistration.ClassID = Class.ID
+                JOIN Users ON ClassRegistration.StudentID = Users.ID
+                JOIN StudentState ON Class.QuarterID = StudentState.QuarterID AND Users.ID = StudentState.StudentID
+            WHERE StudentState.Stage <> \'dropped\' AND Users.UserStatus <> \'terminated\' AND Class.ID = :ID`;
         return Connection.getInstance().query<StudentInClass>(statement,
             {
                 raw: true,
